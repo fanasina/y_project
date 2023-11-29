09c402a4ba722a0c6531391bc94c92e0e6a1ed76
fanasina <rafanasin@gmail.com>
2023-11-29 22:46:44 +0100

    mv tensor repo

--- dimension_t/src/dimension_t.h
+++ dimension_t/src/dimension_t.h
@@ -3,6 +3,9 @@
 
 #include "permutation_t/permutation_t.h"
 
-typedef dimension struct PERMUTATION_TYPE_SIZE_T;
+typedef struct PERMUTATION_TYPE_SIZE_T dimension ;
 
+dimension * create_dim(size_t);
+
+#endif /* __DIMENSION_T__H__ */
 //int compare_dimension(dimension *d1, dimension *d2);
