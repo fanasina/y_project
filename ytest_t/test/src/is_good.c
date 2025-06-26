a1fe7c983d45a9f0c0be64c013504ee4d4f73a60
fanasina <rafanasin@gmail.com>
2025-06-26 06:34:52 +0200

    try to debug Makefile, failed to build libytest.so  via tests

--- ytest_t/test/src/is_good.c
+++ ytest_t/test/src/is_good.c
@@ -90,7 +90,7 @@ TEST(expect){
   int b = 6;
   EXPECT_EQ(a,b);
   //SKIP();
-  SKIP("on skip eq string\n");
+  SKIP("%s\n","on skip eq string");
   EXPECT_EQ_TYPE_STRING("hello","hello");
   float f1 = 1.00019999, f2=1.00019999;
   EXPECT_EQ_TYPE_FLOAT(f1,f2);
